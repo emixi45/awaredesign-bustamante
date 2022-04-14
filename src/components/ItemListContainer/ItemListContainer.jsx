0d64058168c56828952e12e6c9793faedf83a31e
bustamantelucas <bustamante.lucas@hotmail.com.ar>
2022-04-13 21:38:49 -0300

    firebasev1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect,useState } from 'react';
 import ItemList from '../ItemList';
 
 import { useParams } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../firebase/config';
 
 
@@ -15,13 +15,14 @@ const ItemListContainer=()=>{
 	const [ListaProductos,setListaProductos]=useState([])
 	const [Cargando,setCargando]=useState(true)
 	const {categoryId} = useParams()
-	console.log(categoryId)
+	
 	
 useEffect(()=>{
 	setCargando(true)
 
 	const productosRef = collection(db, "productos")
-	getDocs(productosRef)
+	const q = categoryId ? query(productosRef, where('category', '==', categoryId)) : productosRef
+	getDocs(q)
 		.then(resp => {
 			const items= resp.docs.map((doc)=> ({id: doc.id, ...doc.data()}))
 			console.log(items)
