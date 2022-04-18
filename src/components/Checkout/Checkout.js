9071bf25f9e87182e368c046029aa62c9d59877b
bustamantelucas <bustamante.lucas@hotmail.com.ar>
2022-04-18 03:30:14 -0300

    firebasev4

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,8 +1,13 @@
 import React, { useContext, useState } from "react";
 import {CartContext} from "../../context/CartContext"
+import {db} from "../../firebase/config"
+import { collection, addDoc, Timestamp, updateDoc, doc, getDoc } from "firebase/firestore";
+import { Link, Navigate } from "react-router-dom";
+
 const Checkout = ()=> {
-    const {cart,cartTotal} = useContext (CartContext)
+    const {cart,cartTotal,vaciarCarrito} = useContext (CartContext)
     
+    const [orderId, setOrderId]=useState(null)
 
     const [values, setValues]= useState({
         nombre:'',
@@ -21,10 +26,47 @@ const Checkout = ()=> {
         const orden = {
             items:cart,
             total: cartTotal(),
-            comprador: {...values}
+            comprador: {...values},
+            fyh: Timestamp.fromDate(new Date())
         }
+        const ordersRef =collection(db, 'Orders')
+        cart.forEach((item)=>{
+            const docRef = doc(db, 'productos' , item.id)
+            getDoc (docRef)
+            .then((doc)=>{
+                if (doc.data().stock >= item.cantidad) {
+                    updateDoc(docRef, {
+                        stock: doc.data().stock - item.cantidad
+                    })
+                }else {
+                    alert('no hay stock del item')
+                }
+                
+            })
+        })
+        addDoc(ordersRef, orden)
+        .then((doc)=>{
+            
+            console.log(doc.id)
+            setOrderId(doc.id)
+            vaciarCarrito()
+        })
+       
+
         console.log(orden)
         
+    }
+
+    if (orderId){
+        return <div className="container my-5">
+                <h2>Tur orden se registro Exitosamente!!</h2>
+                <hr/>
+                <h4>guarda tu numero de orden es {orderId}</h4>
+                <Link to="/" className="btn btn-primary">Home</Link>
+        </div>
+    }
+    if (cart.length === 0){
+        return <Navigate to='/' />
     }
     return (
         <div>
